d8d875c544b31080d87baece218ebdd1fe4d309a
Samantha Haddock <59586234+haddocksamantha@users.noreply.github.com>
2022-10-12 15:12:56 -0600

    cut script

---
+++ TS_Proj/Assets/Scripts/ScissorsCut.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScissorsCut : MonoBehaviour
+{
+  [SerializeField] private Animator teddyAnim;
+
+  public Animation cutAnim1;
+  private Animation cutAnim2;
+  private Animation cutAnim3;
+
+  private int numOfClicks;
+
+  public GameObject scissors;
+
+  
+
+
+
+  void Start()
+  {
+    scissors = gameObject.GetComponent<GameObject>();
+    numOfClicks = 0;
+
+  }
+
+  private void Update()
+  {
+    CheckForClick();
+  }
+
+  private void CheckForClick()
+  {
+    if(Input.GetMouseButtonDown(0))
+    {
+      numOfClicks ++;
+      if(numOfClicks == 1)
+      {
+        PlayAnim1(); 
+      }else if(numOfClicks == 2)
+      {
+        PlayAnim2();
+      }else if(numOfClicks == 3)
+      {
+        PlayAnim3();
+      }else
+      {
+        Debug.Log("cuts complete");
+      }
+         
+    }
+  }
+
+  private void PlayAnim1()
+  {
+    teddyAnim.Play("Cut01", 0, 0.0f);
+  }
+
+  private void PlayAnim2()
+  {
+    Debug.Log("anim2");
+  }
+
+  private void PlayAnim3()
+  {
+    Debug.Log("anim3");
+  }
+
+
+}
